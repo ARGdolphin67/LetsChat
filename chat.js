bbc172a3e1dec0cf2848ca3bf489e76db7eeb463
ARGdolphin67 <109750556+ARGdolphin67@users.noreply.github.com>
2022-11-29 11:47:26 -0500

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,5 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDcgahiykp4c273xivP84IQlmeqB0y3yRY",
+    authDomain: "letschat-73528.firebaseapp.com",
+    databaseURL: "https://letschat-73528-default-rtdb.firebaseio.com",
+    projectId: "letschat-73528",
+    storageBucket: "letschat-73528.appspot.com",
+    messagingSenderId: "895431128010",
+    appId: "1:895431128010:web:f317773bb02d3991151533"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 function addUser(){
     username=document.getElementById("username").value;
-    localStorage.setItem("username", username);
-    window.location="chat_room.html";
+    firebase.database().ref("/").child(username).update({
+        purpose:"adding user"
+    });
 }
